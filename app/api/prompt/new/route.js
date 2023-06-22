0fc294d9abdb163c67d7ea29f3f349868337f9c7
Oliver Soledad <renoliver0027@gmail.com>
2023-06-22 16:14:55 +0800

    initial commit

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,21 @@
+import { connectDB } from "@utils/database";
+import Prompt from "@models/Prompt";
+
+export const POST = async (request) => {
+  const { userId, prompt, tag } = await request.json();
+
+  try {
+    await connectDB();
+
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    return new Response("Failed to create a new prompt", { status: 500 });
+  }
+};
